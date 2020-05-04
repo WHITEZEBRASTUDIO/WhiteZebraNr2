f84c47b8b02a83bafb511b8ba01b816da87b90ba
BeataBoettcher-Sisak <64478613+BeataBoettcher-Sisak@users.noreply.github.com>
2020-05-04 16:20:45 +0200

    Dori`s code added

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# studio white zebra
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Dora81/pen/ExVwMqG](https://codepen.io/Dora81/pen/ExVwMqG).
+
+
---
+++ src/script.js
@@ -0,0 +1,14 @@
+
+let data = {
+  photo: 'images/photo1.jpg',
+  title: 'My title',
+  description: 'What happened here, why is this a very nice image'
+};
+
+$('#photo').attr('src', data.photo);
+// ...
+
+let currentPhoto = 0;
+let imagesData = [...];
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+// ...
